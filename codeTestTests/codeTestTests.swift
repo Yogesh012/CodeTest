e813218bc8e0d2862eb261d43998491fe5f66f13
Yogesh <yogeshsharma.qg@gmail.com>
2017-05-25 13:30:16 +0530

    Initial Commit

---
+++ codeTestTests/codeTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  codeTestTests.swift
+//  codeTestTests
+//
+//  Created by Yogesh Sharma on 25/05/17.
+//  Copyright © 2017 Yogesh Sharma. All rights reserved.
+//
+
+import XCTest
+@testable import codeTest
+
+class codeTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
